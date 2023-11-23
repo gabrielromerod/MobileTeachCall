84c33a4b71cdd8b8d4571e291223d35d682c0346
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-11-23 05:20:38 -0500

    Pagino student full

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Linking } from 'react-native';
 import { Button, Card } from 'react-native-paper';
 
 const BookingListScreen = () => {
@@ -9,55 +9,51 @@ const BookingListScreen = () => {
 
   useEffect(() => {
     const fetchBookings = async () => {
+      const authToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnYWJvbGFuZGlhMTU2QGdtYWlsLmNvbSIsInJvbGUiOiJST0xFX3N0dWRlbnQiLCJleHAiOjE3MDA3NjY0NjMsImlhdCI6MTcwMDczMDQ2M30.U0ub4ev2t2yNVzAU9ghnTsi1woSqwsA03POPFcfRGm4"; // Asegúrate de que tu token está actualizado
+      const endpoint = "http://192.168.3.7:8080/bookings/student"; // Endpoint para estudiantes
+
       try {
-        const authToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnYWJvbGFuZGlhMTU2QGdtYWlsLmNvbSIsInJvbGUiOiJST0xFX3N0dWRlbnQiLCJleHAiOjE3MDA3NDk5MzQsImlhdCI6MTcwMDcxMzkzNH0.jRA0VNS18bxb0ze72Io_8QXbqWG68J5cQLq2yZa2DT8"; // Asegúrate de que tu token está actualizado
-        const response = await fetch("https://deployteachcall-production.up.railway.app/bookings/student", {
-          headers: {
-            'Authorization': authToken
-          }
+        const response = await fetch(endpoint, {
+          headers: { 'Authorization': authToken }
         });
-  
-        // Verifica si la respuesta es ok antes de proceder
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-  
-        // Asegúrate de que la respuesta tiene contenido antes de analizarla
-        const text = await response.text();
-        if (!text) {
-          throw new Error('No content returned from the API');
-        }
-  
-        const data = JSON.parse(text);
+
+        const data = await response.json();
         setBookings(data.content || []);
         setTotalPages(data.totalPages || 0);
       } catch (error) {
         console.error('Error al obtener los bookings:', error);
       }
     };
-  
+
     fetchBookings();
   }, [page]);
-  
 
   const renderItem = ({ item }) => {
-    // Accediendo directamente a las propiedades que esperamos según la respuesta de la API
-    const title = item.title ?? 'Título no disponible';
-    const dateTime = `${item.date} ${item.startTime}`; // Combinando fecha y hora
-    const studentName = `${item.firstName} ${item.lastName}`;
-    const link = item.link ?? 'Enlace no disponible';
-  
+    const { link, timeSlot, course, professor } = item;
+    const dateTime = timeSlot ? `${timeSlot.date} ${timeSlot.startTime}` : 'No disponible';
+    const professorName = `${professor.firstName} ${professor.lastName}`;
+
     return (
-      <Card>
-        <Card.Title title={title} subtitle={`Hora: ${dateTime}`} />
+      <Card style={styles.card}>
+        <Card.Title title={course.title} subtitle={`Hora: ${dateTime}`} />
         <Card.Content>
-          <Text>{link}</Text>
-          <Text>{`Estudiante: ${studentName}`}</Text>
+          <Text style={styles.text}>{`Profesor: ${professorName}`}</Text>
+          <Button 
+            icon="link" 
+            mode="contained" 
+            style={styles.button}
+            onPress={() => Linking.openURL(link)}>
+            Unirse a la clase
+          </Button>
         </Card.Content>
       </Card>
     );
   };
-  
+
   const handlePreviousPage = () => {
     if (page > 0) {
       setPage(prev => prev - 1);
@@ -71,19 +67,47 @@ const BookingListScreen = () => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={bookings}
         renderItem={renderItem}
         keyExtractor={item => item.id.toString()}
       />
-      <View>
-        <Button disabled={page === 0} onPress={handlePreviousPage}>Anterior</Button>
-        <Text>{`Página ${page + 1} de ${totalPages}`}</Text>
-        <Button disabled={page === totalPages - 1} onPress={handleNextPage}>Siguiente</Button>
+      <View style={styles.pagination}>
+        <Button mode="outlined" disabled={page === 0} onPress={handlePreviousPage}>Anterior</Button>
+        <Text style={styles.pageText}>{`Página ${page + 1} de ${totalPages}`}</Text>
+        <Button mode="outlined" disabled={page === totalPages - 1} onPress={handleNextPage}>Siguiente</Button>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#f0f0f0'
+  },
+  card: {
+    marginVertical: 8,
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+  },
+  text: {
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#007bff',
+  },
+  pagination: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  pageText: {
+    fontSize: 16,
+  }
+});
 
 export default BookingListScreen;
