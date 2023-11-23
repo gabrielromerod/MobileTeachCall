37a12ab6c2ee701572f1fe79fd4e3880b79a04ee
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-11-23 03:52:15 -0500

    Profile Photo Boton

--- components/ProfileImagePicker.js
+++ components/ProfileImagePicker.js
@@ -1,95 +1,95 @@
-import React, { useState } from 'react';
-import { View, Image, Pressable, StyleSheet, Alert, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { Icon } from 'react-native-elements';
 
-const ProfileImagePicker = ({ onImagePicked }) => {
+const ProfileImagePicker = ({ imageUri, onImagePicked }) => {
     const [image, setImage] = useState(null);
+  
     const defaultProfileImage = require('../assets/test.png');
 
+    useEffect(() => {
+        if (imageUri) {
+            setImage(imageUri);
+        }
+    }, [imageUri]);
+    
     const pickImage = async () => {
       const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
       if (permissionResult.granted === false) {
         Alert.alert("Permissions required", "You need to grant gallery permissions to use this feature");
         return;
       }
-
+  
       const pickerResult = await ImagePicker.launchImageLibraryAsync();
       if (pickerResult.canceled === true) {
         return;
       }
-
+  
       const pickedImageUri = pickerResult.assets && pickerResult.assets[0].uri;
       if (pickedImageUri) {
         setImage(pickedImageUri);
-        onImagePicked(pickedImageUri);
-      }
+      }  
     };
-
+  
     const takePhoto = async () => {
       const cameraPermissionResult = await ImagePicker.requestCameraPermissionsAsync();
       if (cameraPermissionResult.granted === false) {
         Alert.alert("Permissions required", "You need to grant camera permissions to use this feature");
         return;
       }
-
+  
       const photoResult = await ImagePicker.launchCameraAsync();
       if (photoResult.canceled === true) {
         return;
       }
-
+  
       const photoUri = photoResult.assets && photoResult.assets[0].uri;
       if (photoUri) {
         setImage(photoUri);
-        onImagePicked(photoUri);
       }
     };
 
     return (
         <View style={styles.container}>
             <Image source={image ? { uri: image } : defaultProfileImage} style={styles.image} />
-            <Pressable onPress={pickImage} style={styles.button}>
-                <Text style={styles.buttonText}>Select a Photo</Text>
-            </Pressable>
-            <Pressable onPress={takePhoto} style={styles.button}>
-                <Text style={styles.buttonText}>Take a Photo</Text>
-            </Pressable>
+            <TouchableOpacity style={styles.iconContainer} onPress={pickImage}>
+                <Icon
+                    name='camera'
+                    type='font-awesome'
+                    color='#fff'
+                    size={22}
+                />
+            </TouchableOpacity>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        position: 'relative',
         alignItems: 'center',
-        padding: 20,
-        backgroundColor: '#f5f5f5', // Un color de fondo claro para un diseño minimalista
+        justifyContent: 'center',
+        marginBottom: 30,
+        top: 30,
     },
     image: {
         width: 150,
         height: 150,
-        borderRadius: 75, // Hacer la imagen redonda
-        marginBottom: 20,
-        borderColor: '#ddd', // Bordes sutiles para la imagen
+        borderRadius: 75,
         borderWidth: 2,
+        borderColor: '#fff',
     },
-    button: {
-        backgroundColor: '#007bff', // Un color sólido para los botones
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        borderRadius: 5,
-        marginVertical: 10,
-        shadowColor: '#000', // Sombra sutil para dar profundidad
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 2,
-        elevation: 2,
+    iconContainer: {
+        position: 'absolute',
+        right: 10,
+        backgroundColor: 'black',
+        borderRadius: 20,
+        padding: 8,
+        elevation: 5,
+        bottom: 5,
+        marginRight: 120,
     },
-    buttonText: {
-        color: 'white', // Texto blanco para contrastar con el fondo del botón
-        fontSize: 16,
-    }
 });
-
 
 export default ProfileImagePicker;
